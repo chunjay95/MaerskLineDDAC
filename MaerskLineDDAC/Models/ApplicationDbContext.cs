44e018ec19e704f783c182d9c572b57c289079ef
ChunJay <ChunJay>
2017-07-10 18:44:35 +0800

    Add project files.

---
+++ MaerskLineDDAC/Models/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MaerskLineDDAC.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<UserTokenCache> UserTokenCacheList { get; set; }
+
+        public System.Data.Entity.DbSet<MaerskLineDDAC.Models.Company> Companies { get; set; }
+
+        public System.Data.Entity.DbSet<MaerskLineDDAC.Models.Container> Containers { get; set; }
+
+        public System.Data.Entity.DbSet<MaerskLineDDAC.Models.Ship> Ships { get; set; }
+
+        public System.Data.Entity.DbSet<MaerskLineDDAC.Models.Yard> Yards { get; set; }
+
+        public System.Data.Entity.DbSet<MaerskLineDDAC.Models.BookingSchedule> BookingSchedules { get; set; }
+    }
+
+    public class UserTokenCache
+    {
+        [Key]
+        public int UserTokenCacheId { get; set; }
+        public string webUserUniqueId { get; set; }
+        public byte[] cacheBits { get; set; }
+        public DateTime LastWrite { get; set; }
+    }
+}
---
+++ MaerskLineDDAC/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+namespace MaerskLineDDAC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
